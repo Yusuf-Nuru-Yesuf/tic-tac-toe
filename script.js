d353a013af578c5eaf04e31b7807a3a1c68f3aaa
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-09-17 11:02:36 +0800

    Add Gameboard module

---
+++ script.js
@@ -0,0 +1,36 @@
+const GameBoard = (function() {
+    let board = [];
+    const ROWS = 3;
+    const COLUMNS = 3;
+
+    function createBoard() {
+        for (let i = 0; i < ROWS; i++) {
+            board[i] = [];
+            for (let j = 0; j < COLUMNS; j++) {
+                board[i][j] = "";
+            }
+        }
+    }
+
+    function getBoard() {
+        if (board.length === 0 ) {
+            createBoard();
+        }
+        return board;
+    }
+
+    function updateBoard(row, column, symbol) {
+        if (row < 0 || row >= ROWS || column < 0 || column >= COLUMNS || board[row][column] !== "" ) {
+            return false;
+        }
+        board[row][column] = symbol;
+        return true;
+    }
+
+    function resetBoard() {
+        createBoard();
+    }
+
+
+    return { getBoard, updateBoard, resetBoard };
+})();
